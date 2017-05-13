8bfdd3786cb70a0be86148a26c76a946859764cc
DESKTOP-7R096V6\iamgy <iamgyu@naver.com>
2017-05-13 14:51:06 +0900

    HelloWorld 추가

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("HelloWorld");
+    }
+}
